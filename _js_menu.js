de205c049fd0bc31ffe046c3fbce1e4cfebe60e5
Cristiano Rodrigues <vladrodrigues2003@gmail.com>
2020-12-15 12:39:44 +0100

    Modificações-finais

--- _js_menu.js
+++
@@ -1,6 +0,0 @@
-const hamburguer = document.querySelector('.hamburger')
-const ul = document.querySelector('.menu-principal ul')
-
-hamburguer.addEventListener('click', ()=> {
-    ul.classList.toggle('active')
-})
